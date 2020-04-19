9541e5a4b72e2e9aa4cdc6b1c5ea770c1daa334a
Kelvin De Los Angeles <kelvin623@gmail.com>
2020-04-19 13:44:18 +0300

    Implementing warehouse copy

--- src/global/PrintTemplates/WarehouseCopy.js
+++ src/global/PrintTemplates/WarehouseCopy.js
@@ -1,257 +1,169 @@
 import React from "react";
-import styled from "styled-components";
-import { connect } from "react-redux";
-import { Order as OrderModel } from "../../Models/Order";
-import moment from "moment";
+import ReactPDF, { Page, Text, View, Document } from "@react-pdf/renderer";
+import { red } from "@material-ui/core/colors";
 
-class WarehouseCopy extends React.Component {
-    cart = () => {
-        try {
-            return !this.props.editedCopy ? this.props.activeOrder.cart : this.props.activeOrder.editedOrder.cart;
-        } catch (error) {
-            return this.props.activeOrder.cart;
-        }
-    };
+const WarehousePrint = () => {
+    return (
+        <Document>
+            <Page size='A4' style={$.page}>
+                <View style={$.contact}>
+                    <Text>Alex Beverage</Text>
+                    <Text>Beer and Soda Distributor</Text>
+                    <Text>1231 Lafayette Avenue</Text>
+                    <Text>Bronx, New York</Text>
+                    <Text>(212) 304-8888</Text>
+                    <Text>(718) 993-5555</Text>
+                </View>
+                <View style={$.heading}>
+                    <Text style={$.heading.customer}> Minus, asperiores?Minus, asperiores?Minus, asperiores?</Text>
+                    <Text style={$.heading.tax}>Tax and Deposits Included</Text>
+                    <View style={$.heading.thanks}>
+                        <Text>Thank you for your order</Text>
+                        <Text style={$.heading.date}>04/17/2020</Text>
+                    </View>
+                    <Text style={$.heading.license}>LIC.NO.CO.</Text>
+                </View>
 
-    render() {
-        const ordersArray = Object.values(this.cart()).map((i) => {
-            const flavorsArray =
-                i.hasOwnProperty("flavorsQuantity") &&
-                Object.entries(i.flavorsQuantity)
-                    .filter((i) => {
-                        return i[1] > 0;
-                    })
-                    .map((i) => {
-                        return (
-                            <Flavor>
-                                <p>{i[1]}</p> <p>x</p> <p> {i[0]}</p>
-                            </Flavor>
-                        );
-                    });
+                <View style={$.wrapper}>
+                    <View style={$.ordersHeader}>
+                        <Text style={$.ordersHeader.w}>WH</Text>
+                        <Text style={$.ordersHeader.c}> Cases</Text>
+                        <Text style={$.ordersHeader.d}>Description</Text>
+                        <Text style={$.ordersHeader.ch}>CHK</Text>
+                    </View>
+                    <View style={$.ordersHeader}>
+                        <Text style={$.ordersHeader.w}>WH</Text>
+                        <Text style={$.ordersHeader.c}> Cases</Text>
+                        <Text style={$.ordersHeader.d}>Description</Text>
+                        <Text style={$.ordersHeader.ch}>CHK</Text>
+                    </View>
 
-            return (
-                <React.Fragment>
-                    <Quantity>_</Quantity>
-                    <p>{i.qty}</p>
-                    <div className='description'>
-                        <p className='price'>{i.description}</p>
-                        {flavorsArray}
-                    </div>
-                    <Quantity>_</Quantity>
-                </React.Fragment>
-            );
-        });
+                    <View style={$.orders}>
+                        <View style={$.orders.w}>
+                            <View style={$.orders.checkBox}></View>
+                        </View>
+                        <Text style={$.ordersHeader.c}> Cases</Text>
+                        <Text style={$.ordersHeader.d}>Description</Text>
+                        <View style={$.orders.w}>
+                            <View style={$.orders.checkBox}></View>
+                        </View>
+                    </View>
 
-        return (
-            <WarehouseCopyWrapper ref={this.props.reference}>
-                <Header>
-                    <h5>Alex Beverage</h5>
-                    <p>Beer &amp; Soda Distributor</p>
-                    <p>1231 Lafayette Avenue</p>
-                    <p>Bronx, New York</p>
-                    <p>(212) 304-8888</p>
-                    <p>(718) 993-5555</p>
-                </Header>
-                <Address>{this.props.activeOrder.customer.address}</Address>
-                <TaxNotice>Tax and Deposits Included</TaxNotice>
-                <SecondaryHeader>
-                    <h3>LIC. NO.CO</h3>
-                    <div>
-                        <h3>Thank you for your order</h3>
-                        <h3>{moment(this.props.activeOrder.details.createdAt).format("MM/DD/2020")}</h3>
-                    </div>
-                </SecondaryHeader>
-                <DBOrderHeader>
-                    <h3>Warehouse</h3>
-                    <h3>Cases</h3>
-                    <h3>Description</h3>
-                    <h3>CHK</h3>
-                </DBOrderHeader>
-                <OrderContainer>
-                    <h3>Warehouse</h3>
-                    <h3>Cases</h3>
-                    <h3>Description</h3>
-                    <h3>CHK</h3>
-                    {ordersArray}
-                </OrderContainer>
-                <Footer>
-                    <div className='order-data'>
-                        {<p>Total Cases : {OrderModel.CalculateCases(this.props.activeOrder.cart)}</p>}
-                        {/* <p>invoice Total: $ {OrderModel.CalculateCart(this.cart())}</p> */}
-                        {/* </div>
-          <p className="credit">Credits ______________________</p>
-          <div className="balances">
-            <p>Balance _____________________</p>
-            <p>Balance _____________________</p>
-          </div>
-          <p className="paid">Amount Paid _____________________</p>
-          <div className="signature">
-            <p>Signature _____________________</p>
-            <p>Date _____________________</p> */}
-                    </div>
-                </Footer>
-            </WarehouseCopyWrapper>
-        );
-    }
-}
+                    <View style={$.orders}>
+                        <View style={$.orders.w}>
+                            <View style={$.orders.checkBox}></View>
+                        </View>
+                        <Text style={$.ordersHeader.c}> Cases</Text>
+                        <Text style={$.ordersHeader.d}>Description</Text>
+                        <View style={$.orders.w}>
+                            <View style={$.orders.checkBox}></View>
+                        </View>
+                    </View>
 
-const WarehouseCopyWrapper = styled.div`
-    /* @media print {
-        body,
-        * {
-            margin: 0;
-            box-shadow: 0;
-            display: table;
-        }
-        div {
-            page-break-inside: avoid;
-        }
-    } */
-    width: 100%;
-    margin: auto;
-    padding: 0 16px;
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-    height: calc(var(--vh, 1vh) * 100);
-    .description .price {
-        margin-bottom: 4px;
-    }
-    h3 {
-        font-family: OpenSans-SemiBold;
-        font-size: 14px;
-        text-align: center;
-        border-radius: 4px;
-    }
-`;
-const Header = styled.header`
-    margin-bottom: 40px;
-    padding: 8px;
-    text-align: center;
-    h5 {
-        font-family: OpenSans-ExtraBold;
-        font-size: 14px;
-        color: #000000;
-        text-align: center;
-        margin-bottom: 4px;
-    }
-    p {
-        font-family: OpenSans-SemiBold;
-        font-size: 14px;
-        color: #000000;
-        text-align: center;
-    }
-`;
-const Address = styled.h3`
-    padding: 16px 18px;
-    align-self: flex-start;
-    margin-bottom: 24px;
-    border: 2px solid black;
-    margin-left: 8px;
-`;
-const TaxNotice = styled.p`
-    font-family: OpenSans-Regular;
-    font-size: 14px;
-    text-align: center;
-    margin-bottom: 24px;
-`;
+                    <View style={$.orders}>
+                        <View style={$.orders.w}>
+                            <View style={$.orders.checkBox}></View>
+                        </View>
+                        <Text style={$.ordersHeader.c}> Cases</Text>
+                        <Text style={$.ordersHeader.d}>Description</Text>
+                        <View style={$.orders.w}>
+                            <View style={$.orders.checkBox}></View>
+                        </View>
+                    </View>
 
-const SecondaryHeader = styled.div`
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 40px;
-    padding: 0 8px;
-    h3 {
-        padding: 0;
-    }
-`;
+                    <View style={$.orders}>
+                        <View style={$.orders.w}>
+                            <View style={$.orders.checkBox}></View>
+                        </View>
+                        <Text style={$.ordersHeader.c}> Cases</Text>
+                        <Text style={$.ordersHeader.d}>Description</Text>
+                        <View style={$.orders.w}>
+                            <View style={$.orders.checkBox}></View>
+                        </View>
+                    </View>
+                </View>
 
-const DBOrderHeader = styled.div`
-    display: grid;
-    grid-gap: 10px 32px;
-    grid-template-columns: 62px 32px minmax(180px, 200px) auto;
-    justify-items: flex-start;
-    padding: 8px;
-    border-top: 1px dotted black;
-    border-bottom: 1px dotted black;
-`;
+                <Text style={$.footer}> Total Cases: 25 </Text>
+            </Page>
+        </Document>
+    );
+};
 
-const OrderContainer = styled.div`
-    display: grid;
-    grid-gap: 0px 32px;
-    grid-template-columns: 62px 32px minmax(180px, 200px) auto;
-    justify-items: flex-start;
+const $ = {
+    page: {
+        fontSize: "10",
+        padding: 16,
+    },
+    contact: {
+        textAlign: "center",
+        marginBottom: "20",
+    },
+    heading: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: "10",
+        customer: {
+            border: "1 solid black",
+            borderRadius: "4",
+            alignSelf: "flex-start",
 
-    align-items: flex-start;
-    padding: 0 8px;
-    font-family: OpenSans-SemiBold;
-    font-size: 14px;
-    h3 {
-        color: transparent;
-        line-height: 0px;
-        margin-bottom: 16px;
-    }
-`;
+            padding: "8",
+            // backgroundColor: 'green',
+            flexBasis: "30%",
+        },
+        tax: {
+            textAlign: "center",
+            flexBasis: "30%",
+        },
+        thanks: {
+            flexBasis: "30%",
+            alignItems: "center",
+        },
+        license: {
+            marginTop: "12",
+            flexBasis: "30%",
+        },
+    },
 
-const Quantity = styled.p`
-    padding: 2px;
-    border: 1px solid black;
-    width: 28px;
-    min-height: 28px;
-    text-align: center;
-    height: fit-content;
-    color: transparent;
-`;
+    wrapper: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+    },
 
-const Footer = styled.footer`
-    margin-top: auto;
-    padding: 0 8px;
-    p {
-        font-family: OpenSans-SemiBold;
-        font-size: 14px;
-    }
-    .order-data {
-        padding-top: 40px;
-        display: flex;
-        justify-content: space-between;
-        margin-bottom: 16px;
-    }
-    .credit {
-        margin-bottom: 16px;
-    }
-    .balances {
-        display: flex;
-        justify-content: space-between;
-        margin-bottom: 16px;
-    }
-    .paid {
-        margin-bottom: 16px;
-    }
-    .signature {
-        display: flex;
-        justify-content: flex-end;
-        margin-bottom: 16px;
-        p {
-            margin-left: 16px;
-        }
-    }
-`;
+    ordersHeader: {
+        width: "50%",
+        flexDirection: "row",
+        border: "1",
+        borderRight: "none",
+        borderLeft: "none",
+        marginBottom: "8",
+        paddingVertical: 4,
+        w: { flexBasis: "10%" },
+        c: { flexBasis: "15%" },
+        d: { flexBasis: "65%" },
+        ch: { flexBasis: "10%" },
+    },
+    orders: {
+        flexDirection: "row",
+        alignItems: "center",
+        width: "50%",
+        marginBottom: 2,
+        w: { flexBasis: "10%" },
+        c: { flexBasis: "15%" },
+        d: { flexBasis: "65%" },
+        ch: { flexBasis: "10%" },
+        checkBox: {
+            height: "20",
+            width: "20",
+            border: "1",
+        },
+    },
+    footer: {
+        marginTop: "auto",
+        fontSize: "16",
+    },
+};
 
-const Flavor = styled.div`
-    padding: 4px;
-    padding-left: 16px;
-    display: flex;
-    p {
-        margin-right: 8px;
-        :first-of-type {
-            min-width: 20px;
-            text-align: right;
-        }
-    }
-`;
-
-export default connect((state) => {
-    return {
-        activeOrder: state.DashboardState.activeOrder,
-    };
-})(WarehouseCopy);
+export default WarehousePrint;
--- src/test/PrintPage.js
+++ src/test/PrintPage.js
@@ -1,27 +1,92 @@
 import React from "react";
 import ReactPDF, { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
+import { red } from "@material-ui/core/colors";
 
 const PrintPage = () => {
     const $ = {
         page: {
-            fontSize: "12",
+            fontSize: "10",
+            padding: 16,
+            // backgroundColor: 'beige',
         },
-        header: {
+        contact: {
             textAlign: "center",
-            marginBottom: "40",
+            marginBottom: "20",
         },
-        customer: {
-            marginLeft: "16",
-            maxWidth: "200",
-            border: "2 solid black",
-            padding: "16",
-            borderRadius: "4",
+        heading: {
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "space-between",
+            // backgroundColor: 'beige',
+            alignItems: "center",
+            marginBottom: "10",
+            customer: {
+                border: "1 solid black",
+                borderRadius: "4",
+                alignSelf: "flex-start",
+
+                padding: "8",
+                // backgroundColor: 'green',
+                flexBasis: "30%",
+            },
+            tax: {
+                textAlign: "center",
+                flexBasis: "30%",
+                // backgroundColor: 'orange',
+            },
+            thanks: {
+                flexBasis: "30%",
+                alignItems: "center",
+            },
+            license: {
+                marginTop: "12",
+                flexBasis: "30%",
+            },
+        },
+
+        wrapper: {
+            flexDirection: "row",
+            flexWrap: "wrap",
+        },
+
+        ordersHeader: {
+            width: "50%",
+            flexDirection: "row",
+            border: "1",
+            borderRight: "none",
+            borderLeft: "none",
+            marginBottom: "8",
+            paddingVertical: 4,
+            w: { flexBasis: "10%" },
+            c: { flexBasis: "15%" },
+            d: { flexBasis: "65%" },
+            ch: { flexBasis: "10%" },
+        },
+        orders: {
+            flexDirection: "row",
+            alignItems: "center",
+            width: "50%",
+            marginBottom: 2,
+            w: { flexBasis: "10%" },
+            c: { flexBasis: "15%" },
+            d: { flexBasis: "65%" },
+            ch: { flexBasis: "10%" },
+            checkBox: {
+                height: "20",
+                width: "20",
+                border: "1",
+            },
+        },
+
+        footer: {
+            marginTop: "auto",
+            fontSize: "16",
         },
     };
     return (
         <Document>
             <Page size='A4' style={$.page}>
-                <View style={$.header}>
+                <View style={$.contact}>
                     <Text>Alex Beverage</Text>
                     <Text>Beer and Soda Distributor</Text>
                     <Text>1231 Lafayette Avenue</Text>
@@ -29,10 +94,85 @@ const PrintPage = () => {
                     <Text>(212) 304-8888</Text>
                     <Text>(718) 993-5555</Text>
                 </View>
-                <Text style={$.customer}>Customer Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, asperiores?</Text>
+
+                <View style={$.heading}>
+                    <Text style={$.heading.customer}> Minus, asperiores?Minus, asperiores?Minus, asperiores?</Text>
+                    <Text style={$.heading.tax}>Tax and Deposits Included</Text>
+                    <View style={$.heading.thanks}>
+                        <Text>Thank you for your order</Text>
+                        <Text style={$.heading.date}>04/17/2020</Text>
+                    </View>
+                    <Text style={$.heading.license}>LIC.NO.CO.</Text>
+                </View>
+
+                <View style={$.wrapper}>
+                    <View style={$.ordersHeader}>
+                        <Text style={$.ordersHeader.w}>WH</Text>
+                        <Text style={$.ordersHeader.c}> Cases</Text>
+                        <Text style={$.ordersHeader.d}>Description</Text>
+                        <Text style={$.ordersHeader.ch}>CHK</Text>
+                    </View>
+                    <View style={$.ordersHeader}>
+                        <Text style={$.ordersHeader.w}>WH</Text>
+                        <Text style={$.ordersHeader.c}> Cases</Text>
+                        <Text style={$.ordersHeader.d}>Description</Text>
+                        <Text style={$.ordersHeader.ch}>CHK</Text>
+                    </View>
+
+                    <View style={$.orders}>
+                        <View style={$.orders.w}>
+                            <View style={$.orders.checkBox}></View>
+                        </View>
+                        <Text style={$.ordersHeader.c}> Cases</Text>
+                        <Text style={$.ordersHeader.d}>Description</Text>
+                        <View style={$.orders.w}>
+                            <View style={$.orders.checkBox}></View>
+                        </View>
+                    </View>
+
+                    <View style={$.orders}>
+                        <View style={$.orders.w}>
+                            <View style={$.orders.checkBox}></View>
+                        </View>
+                        <Text style={$.ordersHeader.c}> Cases</Text>
+                        <Text style={$.ordersHeader.d}>Description</Text>
+                        <View style={$.orders.w}>
+                            <View style={$.orders.checkBox}></View>
+                        </View>
+                    </View>
+
+                    <View style={$.orders}>
+                        <View style={$.orders.w}>
+                            <View style={$.orders.checkBox}></View>
+                        </View>
+                        <Text style={$.ordersHeader.c}> Cases</Text>
+                        <Text style={$.ordersHeader.d}>Description</Text>
+                        <View style={$.orders.w}>
+                            <View style={$.orders.checkBox}></View>
+                        </View>
+                    </View>
+
+                    <View style={$.orders}>
+                        <View style={$.orders.w}>
+                            <View style={$.orders.checkBox}></View>
+                        </View>
+                        <Text style={$.ordersHeader.c}> Cases</Text>
+                        <Text style={$.ordersHeader.d}>Description</Text>
+                        <View style={$.orders.w}>
+                            <View style={$.orders.checkBox}></View>
+                        </View>
+                    </View>
+                </View>
+
+                <Text style={$.footer}> Total Cases: 25 </Text>
             </Page>
         </Document>
     );
 };
+
+// const test = styled.div`
+//     grid-template-columns: 1fr 1fr 1fr;
+//     :nth-of-type() ;
+// `;
 
 export default PrintPage;
