27b3d39ddd9697af8af1dbf08de5b5f0b5caaa1e
Kelvin De Los Angeles <kelvin623@gmail.com>
2020-06-25 20:07:45 +0300

    Delete transaction complete

--- src/redux/actions/RapidOrderActions.js
+++ src/redux/actions/RapidOrderActions.js
@@ -37,42 +37,87 @@ export const cancelOrder = () => {
             });
     };
 };
+// export const deleteOrder = (order, firestore, closeOrderPreview) => {
+//     return (dispatch, getState) => {
+//         const { orderID } = order.details;
+//         const dashboardOrders = getState().Firestore.data.ordersv2.orders;
+//         // destructure the order id out of the orders object to set
+//         const { [orderID]: deleted, ...rest } = dashboardOrders;
+//         // add the order to the deleted orders collection, upon success then remove the order from the orders collection
+//         const deleteFromFirestore = () => {
+//             firestore
+//                 .set({ collection: "deletedOrders", doc: orderID }, order)
+//                 .then(() => {
+//                     console.log("successfully added to the deleted orders collection");
+//                     firestore
+//                         .set(
+//                             {
+//                                 collection: "ordersv2",
+//                                 doc: "orders",
+//                             },
+//                             rest
+//                         )
+//                         .then(() => {
+//                             console.log("successfully deleted");
+//                             // close the order menu once deleted
+//                             closeOrderPreview();
+//                         })
+//                         .catch((err) => {
+//                             console.log(err);
+//                         });
+//                 })
+//                 .catch((err) => {
+//                     console.log(err);
+//                 });
+//         };
+//         window.confirm(`Are you sure you want to delete for ${order.customer.address}`) &&
+//             window.confirm("This action is irreversible, Delete anyway?") &&
+//             deleteFromFirestore();
+//     };
+// };
 export const deleteOrder = (order, firestore, closeOrderPreview) => {
     return (dispatch, getState) => {
-        const { orderID } = order.details;
         const dashboardOrders = getState().Firestore.data.ordersv2.orders;
+        const routes = getState().Firestore.data.routes.routes;
+        const allRoutes = Object.values(routes)
+            .map((x) => x.orders)
+            .flat();
         // destructure the order id out of the orders object to set
-        const { [orderID]: deleted, ...rest } = dashboardOrders;
-        // add the order to the deleted orders collection, upon success then remove the order from the orders collection
-        const deleteFromFirestore = () => {
+        const { [order.details.orderID]: deleted, ...rest } = dashboardOrders;
+        // document Ref
+        const dashboardOrdersRef = firestore.collection("ordersv2").doc("orders");
+        // confirm twice before deleting
+        window.confirm("Are you sure want to delete this order") &&
+            window.confirm("This action is irreversible") &&
+            // firestore Transaction
             firestore
-                .set({ collection: "deletedOrders", doc: orderID }, order)
-                .then(() => {
-                    console.log("successfully added to the deleted orders collection");
-                    firestore
-                        .set(
-                            {
-                                collection: "ordersv2",
-                                doc: "orders",
-                            },
-                            rest
-                        )
-                        .then(() => {
-                            console.log("successfully deleted");
-                            // close the order menu once deleted
-                            closeOrderPreview();
-                        })
-                        .catch((err) => {
-                            console.log(err);
+                .runTransaction((t) => {
+                    // read the dashboard orders before running the transaction
+                    return t.get(dashboardOrdersRef).then((res) => {
+                        // List of Transactions
+                        // write the deleted order to the deletedordersv2 collection
+                        t.set(firestore.collection("deletedOrdersv2").doc(order.details.orderID), order);
+                        // set the dashboard orders list to the orders list excluding the deleted order
+                        t.set(dashboardOrdersRef, rest);
+                        // run a check to see if that order is assigned, if it is then delete that order from route
+                        Object.values(routes).forEach((x) => {
+                            x.orders.includes(order.details.orderID) &&
+                                t.update(firestore.collection("routes").doc("routes"), {
+                                    // returns the route with  a new order list filtering out the deleted order
+                                    [x.details.routeID]: { ...x, orders: x.orders.filter((a) => a !== order.details.orderID) },
+                                });
+                            // TODO:100 potentially add another check to see if the route has only one DataTransferItem, if it does delete the entire route
                         });
+                    });
+                })
+                .then(() => {
+                    console.log("transaction was successful");
+                    closeOrderPreview();
                 })
                 .catch((err) => {
                     console.log(err);
+                    console.log("Transaction not successful");
                 });
-        };
-        window.confirm(`Are you sure you want to delete for ${order.customer.address}`) &&
-            window.confirm("This action is irreversible, Delete anyway?") &&
-            deleteFromFirestore();
     };
 };
 
