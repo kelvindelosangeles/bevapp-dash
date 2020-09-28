410c0fefb4a799d24eedc5f0c768de05e6cf9270
Kelvin De Los Angeles <kelvin623@gmail.com>
2020-09-28 19:07:13 +0300

    Stable

--- src/test/Manual.js
+++ src/test/Manual.js
@@ -7,42 +7,51 @@ import moment from "moment-timezone";
 import { useSelector } from "react-redux";
 
 import { ActionBar, Application, Body } from "../components/layout/Application";
+import { Colors } from "../Constants/Colors";
 
 const ManualTasks = () => {
-    const firestore = useFirestore();
-    // const data = Object.entries(useSelector((state) => state.Firestore.data.collToDelete)).map((x) => x[0]);
-    // console.log(data);
-
-    useEffect(() => {
-        // firestore
-        //     .update({ collection: "ordersv2", doc: "202009365" }, data)
-        //     .then(() => {
-        //         console.log("success");
-        //     })
-        //     .catch((err) => {
-        //         console.log(err);
-        //     });
-        // data.forEach((x) => {
-        //     console.log(x);
-        //     firestore
-        //         .delete({ collection: "orders", doc: x })
-        //         .then(() => {
-        //             console.log("success");
-        //         })
-        //         .catch((err) => {
-        //             console.log(err);
-        //         });
-        // });
-    }, []);
+    const customers = Object.values(useSelector((state) => state.Firestore.data.store.customers))
+        .filter((a) => {
+            return a.hasOwnProperty("specialPrices");
+        })
+        .filter((c) => {
+            return c.specialPrices.hasOwnProperty("COR12B");
+        });
 
     return (
         <Application>
             <ActionBar></ActionBar>
-            <Body title='Manual operation'>this page is used to complete manual operations</Body>
+            <Body title='Customers with SP on COR12B'>
+                <Component>
+                    {customers.map((b) => {
+                        return (
+                            <div className='customer'>
+                                <p>{b.address}</p>
+                                <p>Special Price - ${b.specialPrices["COR12B"].price}</p>
+                                <p></p>
+                            </div>
+                        );
+                    })}
+                </Component>
+            </Body>
         </Application>
     );
 };
 
+const Component = styled.div`
+    display: grid;
+    grid-row-gap: 12px;
+    .customer {
+        padding: 8px 0;
+        :nth-child(even) {
+            background-color: ${Colors.lightGrey};
+        }
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        text-transform: uppercase;
+        font-weight: 600;
+    }
+`;
 export default ManualTasks;
 
 // const nytz = "America/New_York";
@@ -51,3 +60,29 @@ export default ManualTasks;
 // console.log(moment().valueOf());
 // console.log(moment.tz(1598888788848, nytz).format("LLL"));
 // console.log(moment.tz(1598888788848, turtz).format("LLL"));
+
+// const firestore = useFirestore();
+// // const data = Object.entries(useSelector((state) => state.Firestore.data.collToDelete)).map((x) => x[0]);
+// // console.log(data);
+
+// useEffect(() => {
+//     // firestore
+//     //     .update({ collection: "ordersv2", doc: "202009365" }, data)
+//     //     .then(() => {
+//     //         console.log("success");
+//     //     })
+//     //     .catch((err) => {
+//     //         console.log(err);
+//     //     });
+//     // data.forEach((x) => {
+//     //     console.log(x);
+//     //     firestore
+//     //         .delete({ collection: "orders", doc: x })
+//     //         .then(() => {
+//     //             console.log("success");
+//     //         })
+//     //         .catch((err) => {
+//     //             console.log(err);
+//     //         });
+//     // });
+// }, []);
