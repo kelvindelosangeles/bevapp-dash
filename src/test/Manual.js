d8668417e01d6cb32eb0284c2b98ec6bdc8581c2
Kelvin De Los Angeles <kelvin623@gmail.com>
2020-10-19 15:18:51 +0300

    Stbale

--- src/test/Manual.js
+++ src/test/Manual.js
@@ -1,57 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import styled from "styled-components";
 import firebase from "firebase";
 import { useFirestore } from "react-redux-firebase";
 import moment from "moment-timezone";
 import { useSelector } from "react-redux";
-
 import { ActionBar, Application, Body } from "../components/layout/Application";
 import { Colors } from "../Constants/Colors";
 
 const ManualTasks = () => {
-    const customers = Object.values(useSelector((state) => state.Firestore.data.store.customers))
-        .filter((a) => {
-            return a.hasOwnProperty("specialPrices");
-        })
-        .filter((c) => {
-            return c.specialPrices.hasOwnProperty("COR12B");
-        });
-
     return (
         <Application>
             <ActionBar></ActionBar>
             <Body title='Customers with SP on COR12B'>
-                <Component>
-                    {customers.map((b) => {
-                        return (
-                            <div className='customer'>
-                                <p>{b.address}</p>
-                                <p>Special Price - ${b.specialPrices["COR12B"].price}</p>
-                                <p></p>
-                            </div>
-                        );
-                    })}
-                </Component>
+                <Component></Component>
             </Body>
         </Application>
     );
 };
 
-const Component = styled.div`
-    display: grid;
-    grid-row-gap: 12px;
-    .customer {
-        padding: 8px 0;
-        :nth-child(even) {
-            background-color: ${Colors.lightGrey};
-        }
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        text-transform: uppercase;
-        font-weight: 600;
-    }
-`;
+const Component = styled.div``;
 export default ManualTasks;
 
 // const nytz = "America/New_York";
