42ab67e8852c954e78c54976a19dcfd7a8300555
Kelvin De Los Angeles <kelvin623@gmail.com>
2020-11-17 19:02:55 +0300

    stable

---
+++ src/componentsv3/navbar/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+const Navbar = () => {
+    return (
+        <Component>
+            <h3>this is the Navbar</h3>
+        </Component>
+    );
+};
+const Component = styled.div``;
+export default Navbar;
