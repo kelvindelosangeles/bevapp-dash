2be1da42052da10b014b23a12cb417bf145a15b4
Kelvin De Los Angeles <kelvin623@gmail.com>
2021-03-22 14:38:01 +0300

    stable

---
+++ src/v5/pages/AccountOverview/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import styled from "styled-components";
+const AccountOverview = () => {
+    return <Component>account over view page</Component>;
+};
+const Component = styled.div``;
+export default AccountOverview;
